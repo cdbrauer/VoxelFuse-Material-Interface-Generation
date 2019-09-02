4c866bf1e993b39a72e133cbcb74f1a09c70ef81
Cole B <cole.brauer@outlook.com>
2019-09-02 15:23:00 -0700

    Added second processing method and timing code

--- lattice_transition/lattice_element.py
+++ lattice_transition/lattice_element.py
@@ -6,6 +6,7 @@ Dan Aukes, Cole Brauer
 # Import Libraries
 import PyQt5.QtGui as qg
 import sys
+import time
 from voxelfuse.voxel_model import VoxelModel
 from voxelfuse.mesh import Mesh
 from voxelfuse.plot import Plot
@@ -14,15 +15,33 @@ from voxelfuse.plot import Plot
 if __name__=='__main__':
     app1 = qg.QApplication(sys.argv)
 
+    max_radius = 25
+
     # Import Models
     latticeModel = VoxelModel.fromVoxFile('lattice_element_1.vox')
     lattice_size = len(latticeModel.model[0, 0, :, 0])
 
-    # Process Models
-    modelResult = latticeModel.dilate(5)
+    # Process Model - standard dilate command
+    start = time.time()
+    modelResult = VoxelModel.copy(latticeModel)
+    for r in range(max_radius):
+        print(str(r) + '/' + str(max_radius))
+        modelResult = modelResult.dilate()
+        modelResult = modelResult.intersection(latticeModel.getBoundingBox())
+    end = time.time()
+    m1Time = (end - start)
+    print(m1Time)
+
+    # Process Model - large dilate command
+    start = time.time()
+    modelResult2 = latticeModel.dilateLarge(max_radius)
+    modelResult2 = modelResult2.intersection(latticeModel.getBoundingBox())
+    end = time.time()
+    m2Time = (end - start)
+    print(m2Time)
 
     # Create Mesh
-    mesh1 = Mesh.fromVoxelModel(modelResult)
+    mesh1 = Mesh.fromVoxelModel(modelResult2)
 
     # Create Plot
     plot1 = Plot(mesh1)
