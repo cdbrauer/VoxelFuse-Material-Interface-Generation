dbcb019e7d565820bdd5aabe03af6a2cbb00e6c5
Cole B <cole.brauer@outlook.com>
2019-11-21 18:34:52 -0700

    Updated gyroid.py to use new functions

--- gyroid/gyroid.py
+++ gyroid/gyroid.py
@@ -2,45 +2,32 @@
 Copyright 2018
 Dan Aukes, Cole Brauer
 
-Generate coupon for tensile testing
+Generate gyroid model
 """
 
-import os
 import sys
-import time
-import math as m
-
 import PyQt5.QtGui as qg
 from tqdm import tqdm
 
 from voxelfuse.voxel_model import VoxelModel
 from voxelfuse.mesh import Mesh
 from voxelfuse.plot import Plot
-from voxelfuse.primitives import *
-from voxelfuse.voxel_model import Axes
+from voxelfuse.periodic import *
 
 if __name__=='__main__':
     app1 = qg.QApplication(sys.argv)
 
     # Settings
     a = 16                  # Scale (voxels/unit)
-    t = 0.001               # Thickess
     size = (50, 50, 16)     # Volume
 
-    gyroid_element = cuboid(size)
-
-    for x in tqdm(range(size[0]), desc='Generating Gyroid'):
-        for y in range(size[1]):
-            for z in range(size[2]):
-                t_calc = m.sin((2 * m.pi * x)/a) * m.cos((2 * m.pi * y)/a) + m.sin((2 * m.pi * y)/a) * m.cos((2 * m.pi * z)/a) + m.sin((2 * m.pi * z)/a) * m.cos((2 * m.pi * x)/a)
-
-                if t_calc < t:
-                    gyroid_element.voxels[x, y, z] = 1
-                else:
-                    gyroid_element.voxels[x, y, z] = 0
+    #model = gyroid(size, a)
+    #model = schwarzP(size, a)
+    #model = schwarzD(size, a)
+    model = FRD(size, a)
 
     # Create Mesh
-    mesh1 = Mesh.fromVoxelModel(gyroid_element)
+    mesh1 = Mesh.fromVoxelModel(model)
 
     # Create Plot
     plot1 = Plot(mesh1)
